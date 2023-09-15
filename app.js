2158757091f73d6ee3b837cc23220ba46acb9660
gabydelpueche <111470264+gabydelpueche@users.noreply.github.com>
2023-09-15 13:18:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 let tasks = [];
 
 app.set('view engine', 'ejs');
